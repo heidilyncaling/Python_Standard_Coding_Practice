22bdb6e1795b0bb74b4f6ac1ff5ebaaf7741188c
heidilyncaling <calingheidilyn@gmail.com>
2025-03-12 06:12:15 +0000

    remove space in print function

--- equal_if_same_number.py
+++ equal_if_same_number.py
@@ -2,6 +2,6 @@ num1 = float(input("Enter the 1st number:"))
 num2 = float(input("Enter the 2nd number:"))
 
 if num1 == num2:
-    print ("Both numbers are equal.")
+    print("Both numbers are equal.")
 else:
-    print ("Numbers are not equal")
+    print("Numbers are not equal")
