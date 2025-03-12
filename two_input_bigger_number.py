d8980671f0f49d10a4901785b8d464505eb82124
heidilyncaling <calingheidilyn@gmail.com>
2025-03-12 06:11:09 +0000

    removed unnecessary spaces and line

--- two_input_bigger_number.py
+++ two_input_bigger_number.py
@@ -1,11 +1,10 @@
-num1 = float(input ("Enter the 1st number: "))
-num2 = float(input ("Enter the 2nd number: "))
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
 
 if num1 > num2:
     print (num1, " is bigger.")    
 elif num2 > num1:
     print (num2, " is bigger.")
-
 else:
-    print ("Both numbers are equal.")
+    print("Both numbers are equal.")
         
