7852552e7fbdf0cd9e0044f0a0ed8c3b442e2a6c
heidilyncaling <calingheidilyn@gmail.com>
2025-03-12 06:15:18 +0000

    removed and add space to improe readability

--- quotient_no_decimal.py
+++ quotient_no_decimal.py
@@ -1,5 +1,5 @@
-num1 = float(input("enter 1st number:"))
-num2 = float(input("enter 2nd number:"))
+num1 = float(input("enter 1st number: "))
+num2 = float(input("enter 2nd number: "))
 
 quotient = num1 / num2
-print (f"{quotient:.0f}")
+print(f"{quotient:.0f}")
