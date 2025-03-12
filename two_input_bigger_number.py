503d65a7af94f58eb98346c7f9ba5fc0c0cb4a6d
heidilyncaling <calingheidilyn@gmail.com>
2025-03-12 05:25:23 +0000

    fixed, follows coding standard

--- two_input_bigger_number.py
+++ two_input_bigger_number.py
@@ -2,11 +2,10 @@ num1 = float(input ("Enter the 1st number: "))
 num2 = float(input ("Enter the 2nd number: "))
 
 if num1 > num2:
-    print (num1, " is bigger.")
-    
+    print (num1, " is bigger.")    
 elif num2 > num1:
     print (num2, " is bigger.")
-    
+
 else:
     print ("Both numbers are equal.")
         
