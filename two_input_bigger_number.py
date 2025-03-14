f4dc98ce9d20c9e3aafb7d397ff35e82c4378c8f
heidilyncaling <calingheidilyn@gmail.com>
2025-03-14 18:33:23 +0800

    Improved readability and corrected a few codes

--- two_input_bigger_number.py
+++ two_input_bigger_number.py
@@ -1,10 +1,29 @@
+<<<<<<< HEAD
+num1 = float(input ("Enter the 1st number: "))
+num2 = float(input ("Enter the 2nd number: "))
+=======
 num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
+>>>>>>> b0f89f5911ea28606ef8cb45633bed97bac8e060
 
 if num1 > num2:
+<<<<<<< HEAD
+    print (num1, " is bigger.")
+    
+=======
     print (num1, " is bigger.")    
+>>>>>>> b0f89f5911ea28606ef8cb45633bed97bac8e060
 elif num2 > num1:
+<<<<<<< HEAD
     print (num2, " is bigger.")
+    
+=======
+    print (num2, " is bigger.")
+>>>>>>> b0f89f5911ea28606ef8cb45633bed97bac8e060
 else:
+<<<<<<< HEAD
+    print ("Both numbers are equal.")
+=======
     print("Both numbers are equal.")
+>>>>>>> b0f89f5911ea28606ef8cb45633bed97bac8e060
         
